e3ddccc69990b6291fbe47d12e9fcc81a5812b49
Safat99 <sarkersafat99@gmail.com>
2022-12-21 11:32:58 +0600

    add Depot edit Depot api done

--- src/main/java/com/example/inventory_project/controller/DepotController.java
+++ src/main/java/com/example/inventory_project/controller/DepotController.java
@@ -4,9 +4,7 @@ import com.example.inventory_project.entity.Depot;
 import com.example.inventory_project.service.DepotService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class DepotController {
@@ -17,6 +15,10 @@ public class DepotController {
     @PostMapping("/addDepot")
     public ResponseEntity<Depot> addDepot(@RequestBody Depot depot){
         return new ResponseEntity<Depot>(depotService.addDepot(depot), HttpStatus.CREATED);
+    }
+    @PutMapping("/editDepot/{id}")
+    public ResponseEntity<Depot> editDepot(@PathVariable("id") Long id, @RequestBody Depot depot){
+        return new ResponseEntity<Depot>(depotService.editDepot(id,depot), HttpStatus.OK);
     }
 
 }
--- src/main/java/com/example/inventory_project/service/DepotService.java
+++ src/main/java/com/example/inventory_project/service/DepotService.java
@@ -4,6 +4,8 @@ import com.example.inventory_project.entity.Depot;
 import com.example.inventory_project.repository.DepotRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class DepotService {
     private final DepotRepository depotRepository;
@@ -12,5 +14,20 @@ public class DepotService {
     }
     public Depot addDepot(Depot depot){
         return depotRepository.save(depot);
+    }
+
+    public Depot editDepot(Long id, Depot givenDepot){
+        Optional<Depot> depot = depotRepository.findById(id);
+        if (depot.isPresent()){
+            Depot changeDepot = depot.get();
+            changeDepot.setDepotName(givenDepot.getDepotName());
+            changeDepot.setLocation(givenDepot.getLocation());
+            depotRepository.save(changeDepot);
+            return changeDepot;
+        }
+        else{
+            System.out.println("Invalid Id given");
+            return null;
+        }
     }
 }
